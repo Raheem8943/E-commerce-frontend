939a4e228368eee6f777769b50bc4c380f18da2f
Raheem8943 <Raheem318558@gmail.com>
2025-08-07 15:22:35 +0530

    part-2

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,12 +2,16 @@ import "./ProductCard.css";
 export default function ProductCard({ product }) {
   return (
     <div className="product-card">
-      <img src={product.image} alt={product.name} />
-      <h2>{product.title}</h2>
-      <p>{product.description}</p>
-      <p>
-        price: $ <b>{product.price}</b>
-      </p>
+      <div>
+        <img src={product.image} alt={product.name} />
+      </div>
+      <div>
+        <h3>{product.title}</h3>
+        <p className="line-clamp">{product.description}</p>
+        <p>
+          price: $ <b>{product.price}</b>
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,15 +1,17 @@
-import { useActionState } from "react";
+import { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
-
 function ProductList() {
-  const [products, setProducts] = useActionState([]);
+  const [products, setProducts] = useState([]);
   async function fetchProduct() {
     const response = await fetch("https://fakestoreapi.com/products");
+    if (!response.ok) {
+      throw new console.error("Failed to fetch products from API");
+    }
     const data = await response.json();
     setProducts(data);
   }
-  useActionState(() => {
-    fetchProduct();
+  useEffect(() => {
+    fetchProduct(), [];
   });
 
   return (
