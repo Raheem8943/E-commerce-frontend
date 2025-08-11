60bdd921dd37f07e04427965b11ac67791f19561
Raheem8943 <Raheem318558@gmail.com>
2025-08-11 14:54:51 +0530

    part 2.1 error,loading,finally

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,18 +2,29 @@ import { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
 function ProductList() {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   async function fetchProduct() {
-    const response = await fetch("https://fakestoreapi.com/products");
-    if (!response.ok) {
-      throw new console.error("Failed to fetch products from API");
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (!response.ok) throw new Error("Failed to fetch products from API");
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
     }
-    const data = await response.json();
-    setProducts(data);
   }
   useEffect(() => {
     fetchProduct(), [];
   });
 
+  if (isLoading) return <p className="isLoading">LOADING...</p>;
+
+  if (error) return;
+  <p>{error}</p>;
   return (
     <div className="product-list">
       {products.map((product) => {
